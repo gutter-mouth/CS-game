5a37e71f711e5449d8bfc519dafa13c64c15c336
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2022-10-18 01:06:29 +0900

    パラメータ設定実装

--- src/components/pages/games/hiddenPicture/HiddenPictureComponent.tsx
+++ src/components/pages/games/hiddenPicture/HiddenPictureComponent.tsx
@@ -1,11 +1,12 @@
 import { Card } from "@mui/material";
+import { ConfigContent } from "./ConfigContent";
 import { MainContent } from "./MainContent";
-
 
 export const HiddenPictureComponent = () => {
     return (
-        
-        <MainContent />
-        
+        <>
+            <MainContent />
+        </>
+
     );
 }
--- src/components/pages/games/hiddenPicture/MainContent.tsx
+++ src/components/pages/games/hiddenPicture/MainContent.tsx
@@ -1,56 +1,107 @@
-import { Box, CardMedia, Grid, Typography } from "@mui/material";
-import { useGrid } from "./useGrid";
-
+import { useState, useEffect } from "react";
+import { Box, CardMedia, Grid, Typography, Container, TextField, Stack, Button } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form"
 
 export const MainContent = () => {
-    const { gridRow, gridCol, gridState, setGridState } = useGrid();
+    const { register, handleSubmit } = useForm();
+    const [image, setImage] = useState("")
+    const [gridRow, setGridRow] = useState(5);
+    const [gridCol, setGridCol] = useState(5);
+    const [gridState, setGridState] = useState<boolean[]>(Array(gridRow * gridCol).fill(false));
+
+    useEffect(() => {
+        seal();
+    }, [gridRow, gridCol, image]);
+
+    const reveal = () => {
+        setGridState(Array(gridRow * gridCol).fill(true));
+    }
+
+    const seal = () => {
+        setGridState(Array(gridRow * gridCol).fill(false));
+    }
+
+    const onSubmit = (data: any) => {
+        setImage(data.image);
+        setGridRow(Number(data.row));
+        setGridCol(Number(data.col));
+        seal();
+    }
 
     return (
-        <Box
-            sx={{
-                position: "relative",
-                margin: "auto",
-                width: "400px",
-                height: "400px",
-            }}>
-            <CardMedia
+        <>
+            <Container
                 sx={{
-                    position: "absolute",
-                    top: "0",
-                    width: "100%",
-                    height: "100%",
-                    border: 1,
+                    margin: "auto",
+                    width: "600px",
                 }}
-                component="img"
-                image="https://s2.booth.pm/782207e4-481e-47c5-8d4a-1235eb8a2503/3f7494a3-8553-4c55-8fab-386618953003.png" />
-            <Grid
+            >
+                <Stack spacing={3}>
+                    <TextField required label="たてのブロック" defaultValue={gridRow} {...register("row")} />
+                    <TextField required label="よこのブロック" defaultValue={gridCol} {...register("col")} />
+                    <TextField required label="絵のURL" {...register("image")} />
+                    <Stack justifyContent="center" direction="row" spacing={3}>
+                        <Button color="primary" variant="contained" size="large"
+                            onClick={handleSubmit(onSubmit)}>
+                            スタート
+                        </Button>
+                        <Button color="primary" variant="contained" size="large"
+                            onClick={reveal}>
+                            こたえをみる
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Container>
+            <Box
                 sx={{
-                    position: "absolute",
-                    top: "0",
-                    width: "100%",
-                    height: "100%",
-                    // bgcolor: "black",
-                }} columns={gridCol} container={true}>
-                {[...Array(gridRow * gridCol)].map((_, i) => {
-                    return (
-                        <Grid
-                        sx={{
-                            border: 1,
-                            color: "white",
-                            bgcolor: gridState[i] ? "" : "black",
-                        }}
-                            item
-                            xs={1}
-                            onClick={()=>setGridState( gridState.map((state,idx)=>(i==idx ? !state: state)))}
-                        >
-                            <Typography
-                            color="white"
-                            >{!gridState[i] && i}</Typography>
-             
-                        </Grid>
-                    );
-                })}
-            </Grid>
-        </Box>
+                    position: "relative",
+                    margin: "auto",
+                    width: "600px",
+                    height: "600px",
+                    marginY: "30px"
+                }}>
+                <CardMedia
+                    sx={{
+                        position: "absolute",
+                        top: "0",
+                        width: "100p%",
+                        height: "100%",
+                        border: 1,
+                    }}
+                    component="img"
+                    image={image}
+                    alt=""
+                />
+                <Grid
+                    sx={{
+                        position: "absolute",
+                        top: "0",
+                        width: "100%",
+                        height: "100%",
+                        // bgcolor: "black",
+                    }} columns={gridCol} container={true}>
+                    {[...Array(gridRow * gridCol)].map((_, i) => {
+                        return (
+                            <Grid
+                                sx={{
+                                    border: 1,
+                                    color: "white",
+                                    bgcolor: gridState[i] ? "" : "black",
+                                    cursor: "pointer",
+                                }}
+                                item
+                                xs={1}
+                                onClick={() => setGridState(gridState.map((state, idx) => (i == idx ? !state : state)))}
+                            >
+                                <Typography
+                                    color="white"
+                                >{!gridState[i] && i + 1}</Typography>
+
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
+        </>
     );
 };
