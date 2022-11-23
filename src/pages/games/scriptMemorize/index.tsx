41ad313ab32338638cdb62a67597b61fe9f972f2
ktakaham <per.tkhm@gmail.com>
2022-11-23 17:25:38 +0900

    scriptmemorize added

---
+++ src/pages/games/scriptMemorize/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import {ScriptMemorizeComponent} from "@/components/pages/games/scriptMemorize"
+
+const Home: NextPage = () => {
+  return (
+    <ScriptMemorizeComponent/>
+  );
+};
+
+export default Home;
